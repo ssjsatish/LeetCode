824b8ed89aa938953e8b1ab771c5172cb7c40c4d
Satish Patel <SSJ.SATISH@GMAIL.COM>
2020-02-18 14:39:38 +0530

    added all the problems by frequency as of 18 Feb 2020

--- wallAndGates.py
+++ wallAndGates.py
@@ -20,5 +20,26 @@ def wallsAndGates(self, rooms):
             rooms[r][c] = rooms[r][c] + 1
             q.append((r,c))
 
-
-
+from collections import deque
+def wallsAndGatesV2(srooms):
+    '''
+    Do not return anything, modify room in-place instead.
+    '''
+    if not rooms:
+        return
+    directions = [(1,0),(-1,0),(0,1),(0,-1)]
+    rows, cols = len(rooms), len(rooms[0])
+    q = deque()
+    for r in range(rows):
+        for c in range(cols):
+            if rooms[r][c] == 0:
+                q.append((r,c))
+    while q:
+        row, col = q.popleft()
+        dist = rooms[row][col] + 1
+        for dy,dx in directions:
+            r = row + dy
+            c = col + dx
+            if r >=0 and 0 <= c < cols and rooms[row][col] == 2147483647:
+                rooms[row][col] = dist
+                q.append((row,col))
